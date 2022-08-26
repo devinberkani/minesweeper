1d6b47c9ce3520c747317f9864e0f0aeeeb09bf6
Devin Berkani <devinberkani@gmail.com>
2022-08-26 14:22:17 -0400

    implement functionality to update game board with number of mines in a given area

--- Minesweeper/src/GameBoard.java
+++ Minesweeper/src/GameBoard.java
@@ -14,6 +14,7 @@ public class GameBoard {
     public GameBoard() {
         getUserInput();
         initializeGameBoard();
+        updateGameBoardWithMines();
         printGameBoard();
     }
 
@@ -39,7 +40,7 @@ public class GameBoard {
             rowTracker++;
             System.out.print(getGameBoard()[i]);
             // print new line for all but the last row
-            if (rowTracker % 9 == 0 && i != getGameBoard().length - 1) {
+            if (rowTracker % gameBoardWidth == 0 && i != getGameBoard().length - 1) {
                 System.out.println();
             }
         }
@@ -73,6 +74,252 @@ public class GameBoard {
         setMineIndices(newMineIndices);
     }
 
+    private void updateGameBoardWithMines() {
+        char[] updatedGameBoard = new char[gameBoardWidth * gameBoardHeight];
+
+        for (int i = 0; i < updatedGameBoard.length; i++) {
+            if (getGameBoard()[i] == 'X') {
+                updatedGameBoard[i] = getGameBoard()[i];
+            } else {
+                int numOfMines = getNumberOfMines(i);
+                updatedGameBoard[i] = numOfMines == 0 ? getGameBoard()[i] : (char) (numOfMines + '0'); // converts int to char
+            }
+        }
+
+        setGameBoard(updatedGameBoard);
+    }
+
+    // calculate number of mines around each empty cell
+    private int getNumberOfMines(int currentIndex) {
+        // check if index is corner - check 3 cells
+        boolean isCorner = isCorner(currentIndex);
+
+        // check if index is side - check 5 cells
+        boolean isSide = isSide(currentIndex);
+
+        // else is middle - check 8 cells
+
+        String exactLocation = findLocation(isCorner, isSide, currentIndex);
+
+        return minesAroundLocation(exactLocation, currentIndex);
+    }
+
+    private int minesAroundLocation(String exactLocation, int currentIndex) {
+
+        int mineCount = 0;
+
+        // corners
+        if (exactLocation.equals("topleft")) {
+            if (getGameBoard()[1] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[gameBoardWidth] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[gameBoardWidth + 1] == 'X') {
+                mineCount++;
+            }
+        } else if (exactLocation.equals("topright")) {
+            if (getGameBoard()[gameBoardWidth - 2] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[gameBoardWidth + gameBoardWidth - 2] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[gameBoardWidth + gameBoardWidth - 1] == 'X') {
+                mineCount++;
+            }
+        } else if (exactLocation.equals("bottomleft")) {
+            if (getGameBoard()[getGameBoard().length - (gameBoardWidth * 2)] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[getGameBoard().length - (gameBoardWidth * 2) + 1] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[(getGameBoard().length - gameBoardWidth) + 1] == 'X') {
+                mineCount++;
+            }
+        } else if (exactLocation.equals("bottomright")) {
+            if (getGameBoard()[getGameBoard().length - 2] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[getGameBoard().length - (gameBoardWidth + 2)] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[getGameBoard().length - (gameBoardWidth + 1)] == 'X') {
+                mineCount++;
+            }
+        // sides
+        } else if (exactLocation.equals("leftside")) {
+            if (getGameBoard()[currentIndex - gameBoardWidth] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[(currentIndex - gameBoardWidth) + 1] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[currentIndex + 1] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[currentIndex + gameBoardWidth] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[(currentIndex + gameBoardWidth) + 1] == 'X') {
+                mineCount++;
+            }
+        } else if (exactLocation.equals("rightside")) {
+            if (getGameBoard()[currentIndex - gameBoardWidth] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[currentIndex - (gameBoardWidth + 1)] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[currentIndex - 1] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[(currentIndex + gameBoardWidth) - 1] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[currentIndex + gameBoardWidth] == 'X') {
+                mineCount++;
+            }
+        } else if (exactLocation.equals("topside")) {
+            if (getGameBoard()[currentIndex - 1] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[(currentIndex + gameBoardWidth) - 1] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[currentIndex + gameBoardWidth] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[(currentIndex + gameBoardWidth) + 1] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[currentIndex + 1] == 'X') {
+                mineCount++;
+            }
+        } else if (exactLocation.equals("bottomside")) {
+            if (getGameBoard()[currentIndex - 1] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[(currentIndex - gameBoardWidth) - 1] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[currentIndex - gameBoardWidth] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[(currentIndex - gameBoardWidth) + 1] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[currentIndex + 1] == 'X') {
+                mineCount++;
+            }
+        // middle
+        } else if (exactLocation.equals("middle")) {
+            if (getGameBoard()[currentIndex - 1] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[(currentIndex - gameBoardWidth) - 1] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[currentIndex - gameBoardWidth] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[(currentIndex - gameBoardWidth) + 1] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[currentIndex + 1] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[(currentIndex + gameBoardWidth) + 1] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[currentIndex + gameBoardWidth] == 'X') {
+                mineCount++;
+            }
+            if (getGameBoard()[(currentIndex + gameBoardWidth) - 1] == 'X') {
+                mineCount++;
+            }
+        }
+        return mineCount;
+    }
+
+    private String findLocation(boolean isCorner, boolean isSide, int currentIndex) {
+
+        // corner locations
+        if (isCorner) {
+            // top left corner
+            if (currentIndex == 0) {
+                return "topleft";
+            // top right corner
+            } else if (currentIndex == gameBoardWidth - 1) {
+                return "topright";
+            // bottom left corner
+            } else if (currentIndex == getGameBoard().length - gameBoardWidth) {
+                return "bottomleft";
+            // bottom right corner
+            } else if (currentIndex == getGameBoard().length - 1) {
+                return "bottomright";
+            }
+        // side locations
+        } else if (isSide) {
+            // left side
+            if (currentIndex % gameBoardWidth == 0) {
+                return "leftside";
+            // right side
+            } else if ((currentIndex - (gameBoardWidth - 1)) % gameBoardWidth == 0) {
+                return "rightside";
+            // top side
+            } else if (currentIndex > 0 && currentIndex < gameBoardWidth) {
+                return "topside";
+            // bottom side
+            } else if (currentIndex > getGameBoard().length - gameBoardWidth && currentIndex < getGameBoard().length - 1) {
+                return "bottomside";
+            }
+        }
+        return "middle";
+    }
+
+    private boolean isCorner(int currentIndex) {
+        // top left corner
+        if (currentIndex == 0) {
+            return true;
+        // top right corner
+        } else if (currentIndex == gameBoardWidth - 1) {
+            return true;
+        // bottom left corner
+        } else if (currentIndex == getGameBoard().length - gameBoardWidth) {
+            return true;
+        // bottom right corner
+        } else if (currentIndex == getGameBoard().length - 1) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isSide(int currentIndex) {
+
+        // return false no matter what if index is a corner
+        if (isCorner(currentIndex)) {
+            return false;
+        }
+
+        // left side
+        if (currentIndex % gameBoardWidth == 0) {
+            return true;
+        // right side
+        } else if ((currentIndex - (gameBoardWidth - 1)) % gameBoardWidth == 0) {
+            return true;
+        // top side
+        } else if (currentIndex > 0 && currentIndex < gameBoardWidth) {
+            return true;
+        // bottom side
+        } else if (currentIndex > getGameBoard().length - gameBoardWidth && currentIndex < getGameBoard().length - 1) {
+            return true;
+        }
+        return false;
+    }
+
     // getters and setters
 
     private char[] getGameBoard() {
@@ -91,5 +338,3 @@ public class GameBoard {
         this.mineIndices = mineIndices;
     }
 }
-
-
